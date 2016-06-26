534e82a7767d41ee1aacdfeb1ac059f51b356680
mpppk <niboshiporipori@gmail.com>
2016-06-26 16:49:14 +0900

    バッジ外をタップしても操作ができない問題を修正
    windowManagerを全画面に表示するのではなく、ドラッグに合わせてwindowManagerを位置をずらすようにした

--- app/src/main/java/com/projegrid/mobile/BadgeService.java
+++ app/src/main/java/com/projegrid/mobile/BadgeService.java
@@ -6,20 +6,23 @@ import android.content.Intent;
 import android.graphics.PixelFormat;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
-
-
 
 /**
  * Created by yuki on 6/23/16.
  */
 public class BadgeService extends Service implements View.OnTouchListener {
     View view;
+    ViewGroup badgeViewGroup;
     WindowManager wm;
     final String SERVICE_NAME = "BadgeService";
+    WindowManager.LayoutParams params;
 
     // ドラッグ中に移動量を取得するための変数
     private int oldx;
@@ -33,21 +36,23 @@ public class BadgeService extends Service implements View.OnTouchListener {
         LayoutInflater layoutInflater = LayoutInflater.from(this);
 
         // 重ね合わせするViewの設定を行う
-        WindowManager.LayoutParams params = new WindowManager.LayoutParams(
-                WindowManager.LayoutParams.MATCH_PARENT,
-                WindowManager.LayoutParams.MATCH_PARENT,
+        params = new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
                 WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,
-                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE |
-                        WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
                 PixelFormat.TRANSLUCENT);
+        params.x = 0;
+        params.y = 0;
+        params.gravity = Gravity.TOP | Gravity.LEFT;
 
         // WindowManagerを取得する
         wm = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
 
         // レイアウトファイルから重ね合わせするViewを作成する
         view = layoutInflater.inflate(R.layout.overlay, null);
-        View newView = view.findViewById(R.id.badge);
-        newView.setOnTouchListener(this);
+        badgeViewGroup = (ViewGroup)view.findViewById(R.id.badge);
+        badgeViewGroup.setOnTouchListener(this);
 
         // Viewを画面上に重ね合わせする
         wm.addView(view, params);
@@ -68,18 +73,29 @@ public class BadgeService extends Service implements View.OnTouchListener {
 
         switch (event.getAction()) {
             case MotionEvent.ACTION_MOVE:
+
                 // 今回イベントでのView移動先の位置
                 int left = v.getLeft() + (x - oldx);
                 int top = v.getTop() + (y - oldy);
                 // Viewを移動する
                 v.layout(left, top, left + v.getWidth(), top
                         + v.getHeight());
+
+                params.x += left;
+                params.y += top;
+                wm.updateViewLayout(view, params);
+
                 break;
         }
 
         // 今回のタッチ位置を保持
         oldx = x;
         oldy = y;
+
+        // 吹き出しに現在位置を表示
+//        BubbleTextVew bubble = (BubbleTextVew) v.findViewById(R.id.badge_bubble);
+//        bubble.setText("x:" + x + " y:" + y);
+
         // イベント処理完了
         return true;
     }
