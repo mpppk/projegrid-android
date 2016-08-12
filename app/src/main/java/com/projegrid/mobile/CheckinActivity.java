b80578242635ba6bf2fd36f10d843a06bf9fe959
mpppk <niboshiporipori@gmail.com>
2016-08-13 01:30:44 +0900

    QRコード読み込み時の匿名ログインを実装

--- app/src/main/java/com/projegrid/mobile/CheckinActivity.java
+++ app/src/main/java/com/projegrid/mobile/CheckinActivity.java
@@ -2,13 +2,23 @@ package com.projegrid.mobile;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class CheckinActivity extends AppCompatActivity {
     private String TAG = "CheckinActivity";
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +38,42 @@ public class CheckinActivity extends AppCompatActivity {
             if (uri != null) {
                 String screenId = uri.getQueryParameter("screenid");
                 String screenToken = uri.getQueryParameter("screentoken");
+
+                mAuth = FirebaseAuth.getInstance();
+
+                mAuthListener = new FirebaseAuth.AuthStateListener() {
+                    @Override
+                    public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                        FirebaseUser user = firebaseAuth.getCurrentUser();
+                        if (user != null) {
+                            // User is signed in
+                            Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                        } else {
+                            // User is signed out
+                            Log.d(TAG, "onAuthStateChanged:signed_out");
+                        }
+                    }
+                };
+
+                mAuth.signInAnonymously()
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                Log.d(TAG, "signInAnonymously:onComplete:" + task.isSuccessful());
+
+                                // If sign in fails, display a message to the user. If sign in succeeds
+                                // the auth state listener will be notified and logic to handle the
+                                // signed in user can be handled in the listener.
+                                if (!task.isSuccessful()) {
+                                    Log.w(TAG, "signInAnonymously", task.getException());
+                                    Toast.makeText(CheckinActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+
+                // checkin apiを叩く
+                
             }
         }
     }
